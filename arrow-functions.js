177099f0811a2c0fe99bef15b83d76f4d7d8938d
caketaster <caketaster@yahoo.co.uk>
2023-08-13 08:59:13 +0000

    Arrow function practice

--- arrow-functions.js
+++ arrow-functions.js
@@ -3,12 +3,38 @@
  * command node arrow-functions.js in the terminal
  */
 
-// Vanilla JavaScript Function
+// // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b
+// }
+// let sum = addTwoNumbers(3, 6);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b
+}
+let sum = addTwoNumbers(3, 6);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers = (a, b) => a + b;
+const addTwoNumbers = (a, b) => (a + b);
+let sum = addTwoNumbers(3, 6);
+console.log(sum);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Whaddup?')
+
+const sayHello = () => console.log('Hello');
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+    This is a multiline string.
+    </p>`
+)
